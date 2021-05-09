62cd46b4f876aa0bce26c61445992959fc071b01
SorrentinoFabiano <fabiano.sorrentino@gmail.com>
2021-05-09 10:09:46 -0300

    trocaPc

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,10 @@
+import Routes from "Routes";
 import BarChart from "components/BarChat";
 import DataTable from "components/DataTable";
 import DonutChart from "components/DonutChart";
 import Footer from "components/Footer";
 import NavBar from "components/NavBar";
-import Routes from "Routes";
+
 
 
 function App() {
