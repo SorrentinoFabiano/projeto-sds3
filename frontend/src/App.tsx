ec4be72154808b45ca9796a14c403c6560a20928
SorrentinoFabiano <fabiano.sorrentino@gmail.com>
2021-05-09 15:44:30 -0300

    Correcao BarChart

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,4 @@
 import Routes from "Routes";
-import BarChart from "components/BarChat";
-import DataTable from "components/DataTable";
-import DonutChart from "components/DonutChart";
-import Footer from "components/Footer";
-import NavBar from "components/NavBar";
-
-
 
 function App() {
   return (
